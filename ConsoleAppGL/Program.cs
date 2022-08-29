bf484892f5603efe5513bf66a2a6969bc64a382e
damian <damian.hallbauer@gmail.com>
2022-08-29 06:36:00 -0600

    add cooment sometimes hiding console donesnt work.. i dont need to add path on windows .. its flaky tho

--- ConsoleAppGL/Program.cs
+++ ConsoleAppGL/Program.cs
@@ -1,11 +1,15 @@
 ﻿
 using MGCore;
-
 {
 
     System.Diagnostics.ProcessStartInfo start =
       new System.Diagnostics.ProcessStartInfo();
-    start.FileName=@"\ConsoleAppGL.exe";
+
+  //  var path = Assembly.GetEntryAssembly()?.Location;  //might need the exe path on some system.. this is close..
+
+    start.FileName=
+        //path +
+        @"\ConsoleAppGL.exe";
     start.WindowStyle=System.Diagnostics.ProcessWindowStyle.Hidden; //Hides GUI
     start.CreateNoWindow=true; //Hides console
     var game = new CoreGame();
