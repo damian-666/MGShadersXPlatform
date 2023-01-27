fb08e862ee0ace3557404bde265e27e9f99db09e
Guillermo689 <guillermo.rodriguez2202@gmail.com>
2023-01-27 15:50:28 -0300

    Renamed the effects with prefix Effect and the type of effect it does. Restored the InvertEffect.fx but will not be used, that code doesn't work, the correct effect to use will be EffectInvert.fx, so it would be better to delete it anyway.

--- MGCore/DrawTests/Invert.cs
+++ MGCore/DrawTests/Invert.cs
@@ -14,7 +14,7 @@ namespace MGCore.DrawTests
         float TotalSeconds;
         SpriteBatch spriteBatch;
         Texture2D texture;
-        Effect _testEffect, _testEffect2, _testEffect3, _testEffect4, _testEffect5;
+        Effect _effectGrayscale, _effectFading, _effectPixelated, _effectTeleport, _effectInvert;
         private Vector2 _pos00, _pos01, _pos02, _pos03, _pos04, _pos05;
         private float _amount = 1;
         private float _dir = -1;
@@ -23,11 +23,11 @@ namespace MGCore.DrawTests
         {
             texture = cm.Load<Texture2D>("orb-red");
             spriteBatch = new SpriteBatch(dev);
-            _testEffect = cm.Load<Effect>("TestEffect");
-            _testEffect2 = cm.Load<Effect>("TestEffect2");
-            _testEffect3 = cm.Load<Effect>("TestEffect3");
-            _testEffect4 = cm.Load<Effect>("TestEffect4");
-            _testEffect5 = cm.Load<Effect>("TestEffect5");
+            _effectGrayscale = cm.Load<Effect>("EffectGrayscale");
+            _effectFading = cm.Load<Effect>("EffectFading");
+            _effectPixelated = cm.Load<Effect>("EffectPixelated");
+            _effectTeleport = cm.Load<Effect>("EffectTeleport");
+            _effectInvert = cm.Load<Effect>("EffectInvert");
 
          
             _pos00 = new(100, 100);
@@ -43,7 +43,7 @@ namespace MGCore.DrawTests
         {
             TotalSeconds = (float)gt.ElapsedGameTime.TotalSeconds;
             _amount += TotalSeconds * _dir;
-            _testEffect4.Parameters["amount"].SetValue(_amount);
+            _effectTeleport.Parameters["amount"].SetValue(_amount);
         }
 
         public void Draw(GameTime time)
@@ -62,31 +62,31 @@ namespace MGCore.DrawTests
             spriteBatch.End();
           //Draw Grayscale image
 
-            spriteBatch.Begin(effect: _testEffect);
+            spriteBatch.Begin(effect: _effectGrayscale);
             spriteBatch.Draw(texture, _pos01, Color.White);
             spriteBatch.End();
 
             //Draw a fading image
 
-            spriteBatch.Begin(effect: _testEffect2);
+            spriteBatch.Begin(effect: _effectFading);
             spriteBatch.Draw(texture, _pos02, Color.White);
             spriteBatch.End();
 
             //Draw a pixelated image
 
-            spriteBatch.Begin(effect: _testEffect3);
+            spriteBatch.Begin(effect: _effectPixelated);
             spriteBatch.Draw(texture, _pos03, Color.White);
             spriteBatch.End();
 
             //Draw a teleport effect image
 
-            spriteBatch.Begin(effect: _testEffect4);
+            spriteBatch.Begin(effect: _effectTeleport);
             spriteBatch.Draw(texture, _pos04, Color.White);
             spriteBatch.End();
 
             //Draw an image with colors inverted
 
-            spriteBatch.Begin(effect: _testEffect5);
+            spriteBatch.Begin(effect: _effectInvert);
             spriteBatch.Draw(texture, _pos05, Color.White);
             spriteBatch.End();
 
---
+++ MGCore/DrawTests/InvertEffect.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class InvertEffect : Effect
+{
+    public InvertEffect(GraphicsDevice device) : base(device, GenerateShaderCode())
+    {
+    }
+
+    private static byte[] GenerateShaderCode()
+    {
+        return new EffectCompiler().Compile(
+            new EffectCompilerInput("", "", "", "ps_4_0"),
+            new EffectCompilerOutput(EffectCompilerTarget.PixelShader));
+    }
+
+    public override void Apply()
+    {
+        GraphicsDevice.Textures[0] = Texture;
+
+        base.Apply();
+    }
+}
